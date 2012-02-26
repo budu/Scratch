21fdff250e897be3c0269d789a20b541e8de7a22
Nicolas Buduroi <nbuduroi@gmail.com>
2012-02-26 17:44:52 -0500

    Added `fetch_or` core extension method to Hash with tests.

--- lib/scratch/core_ext/hash.rb
+++ lib/scratch/core_ext/hash.rb
@@ -1,6 +1,21 @@
 
 Hash.class_eval do
 
+  def fetch_or(*keys)
+    options = keys.extract_options!
+
+    keys.each do |key|
+      begin
+        return fetch(key)
+      rescue
+        next
+      end
+    end
+
+    return options[:default] if options[:default]
+    raise KeyError, ("keys not found: %s" % keys.inspect)
+  end
+
   def invert_splat
     reduce({}) do |memo, (k, vs)|
       if vs.respond_to? :each
--- test/unit/core_ext/hash_test.rb
+++ test/unit/core_ext/hash_test.rb
@@ -9,6 +9,13 @@ class Scratch::CoreExt::HashTest < Test::Unit::TestCase
     @is = { 1 => :a, 2 => :a, 3 => :a, 4 => :b, 5 => :b }
   end
 
+  def test_fetch_or
+    assert_equal :a, @is.fetch_or(1)
+    assert_equal :b, @is.fetch_or(7, 6, 5)
+    assert_equal :c, @is.fetch_or(9, 8, default: :c)
+    assert_raise(KeyError) { @is.fetch_or(10) }
+  end
+
   def test_invert_splat
     assert_equal @is, @h.invert_splat
   end
