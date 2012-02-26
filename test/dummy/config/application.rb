8ddacd993996da76acfac8a04ade554c786e1668
Nicolas Buduroi <nbuduroi@gmail.com>
2012-02-26 00:28:29 -0500

    Changed the test dummy application configuration to not generate tests or stylesheets.

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -51,6 +51,11 @@ module Dummy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.stylesheets false
+      g.test_framework nil
+    end
   end
 end
 
